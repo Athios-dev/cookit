9ace13ee50885214bfbe2ef821721c10b69cbe76
Thibaud d'Arros <thibaud.arros@gmail.com>
2020-06-15 01:48:18 +0200

    Improve segmentation

--- cookit.js
+++ cookit.js
@@ -11,7 +11,10 @@
       buttonColor: '#fad04c',
       buttonText: "I accept",
     }, options);
-
+    
+    const banner = $("<div id='cookit-banner'></div>");
+    const container = $("<div id='cookit-container'></div>");
+    var message = link = button = null;
     const hasCookieConsent = getCookie('cookies-consent');
 
     if (!hasCookieConsent) {
@@ -23,60 +26,52 @@
       });
     }
 
+    function createBanner() {
+      message = $("<p id='cookit-message'>"+ settings.messageText +"</p>");
+      link = $("<a id='cookit-link' href='" + settings.linkUrl + "' target='_blank'>" + settings.linkText + "</a>");
+      button = $("<a id='cookit-button' href='#'>" + settings.buttonText + "</a>");
+    
+      $('body').append(banner);
+      banner.append(container);
+      container.append(message);
+      container.append(link);
+      container.append(button);
+
+      customize();
+    }
+
+    function customize() {
+      banner.css({'background-color': settings.backgroundColor});
+      message.css({'color': settings.messageColor});
+      link.css({'color': settings.linkColor});
+      button.css({
+        'background-color': settings.buttonColor,
+        'color': settings.backgroundColor
+      });
+    }
+    
+    function getCookie(name) {
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const ca = decodedCookie.split(';');
+      name = name + "=";
+    
+      for(let i = 0; i < ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+          }
+      }
+    }
+    
+    function setCookie(name, value, days) {
+      const date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      const expires = "expires="+ date.toUTCString();
+      document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
   };
 })(jQuery);
-
-function createBanner(settings) {
-  var banner = $("<div></div>");
-  banner.attr("id", "cookit-banner").css({
-    'background-color': settings.backgroundColor
-  });
-
-  var container = $("<div></div>");
-  container.attr("id", "cookit-container");
-
-  var message = $("<p>"+ settings.messageText +"</p>");
-  message.attr("id", "cookit-message").css({
-    'color': settings.messageColor
-  });
-
-  var link = $("<a href='" + settings.linkUrl + "' target='_blank'>" + settings.linkText + "</a>");
-  link.attr("id", "cookit-link").css({
-    'color': settings.linkColor
-  });
-
-  var button = $("<a href='#'>" + settings.buttonText + "</a>");
-  button.attr("id", "cookit-button").css({
-    'background-color': settings.buttonColor,
-    'color': settings.backgroundColor
-  });
-
-  $('body').append(banner);
-  banner.append(container);
-  container.append(message);
-  container.append(link);
-  container.append(button);
-}
-
-function getCookie(name) {
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const ca = decodedCookie.split(';');
-  name = name + "=";
-
-  for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
-  }
-}
-
-function setCookie(name, value, days) {
-  const date = new Date();
-  date.setTime(date.getTime() + (days*24*60*60*1000));
-  const expires = "expires="+ date.toUTCString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
