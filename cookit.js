12257b37541445e8443a65ab3a254a7a6b921344
Thibaud d'Arros <thibaud.arros@gmail.com>
2020-06-15 15:23:53 +0200

    Update cookit.js

--- cookit.js
+++ cookit.js
@@ -69,7 +69,7 @@
     function setCookie(name, value, days) {
       const date = new Date();
       date.setTime(date.getTime() + (days*24*60*60*1000));
-      const expires = "expires="+ date.toUTCString();
+      const expires = "expires=" + date.toUTCString();
       document.cookie = name + "=" + value + ";" + expires + ";path=/";
     }
 
